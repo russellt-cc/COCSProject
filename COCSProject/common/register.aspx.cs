8c545e3d05c18596bbeccbcbfdc60c442aef6fc6
NotNerty <trentonr@gmail.com>
2020-11-23 16:28:27 -0500

    Added comments

--- COCSProject/common/register.aspx.cs
+++ COCSProject/common/register.aspx.cs
@@ -10,6 +10,7 @@ namespace COCSProject
 {
     public partial class register : System.Web.UI.Page
     {
+        // Connection string
         private string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=CateringSystemT02;Integrated Security=True";
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -35,18 +36,23 @@ namespace COCSProject
 
         private void CreateNewCatererEntry(string name)
         {
+            // Creating and opening connection to db
             SqlConnection cnn = new SqlConnection(connectionString);
             cnn.Open();
             SqlCommand command;
             SqlDataAdapter adapter = new SqlDataAdapter();
 
+            // Make insert query
             string sql = "Insert into Caterers (Caterer_Name) values('" + name + "')";
 
+            // Initialize command object
             command = new SqlCommand(sql, cnn);
 
+            // Execute command
             adapter.InsertCommand = command;
             adapter.InsertCommand.ExecuteNonQuery();
 
+            // Cleanup
             command.Dispose();
             cnn.Close();
         }
