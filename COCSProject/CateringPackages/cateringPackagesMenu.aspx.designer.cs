4d9eaf81d6587ddedcf374451d9ea4f59f9da286
Seth Rojik <srojik@confederationcollege.ca>
2020-11-23 18:54:40 -0500

    Added buttons to menu items and connected the database

--- COCSProject/CateringPackages/cateringPackagesMenu.aspx.designer.cs
+++ COCSProject/CateringPackages/cateringPackagesMenu.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace COCSProject.common {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// dataListPackages control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList dataListPackages;
+        
+        /// <summary>
+        /// dataSourceCaterers control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource dataSourceCaterers;
     }
 }
